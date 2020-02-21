34ed0ef5030fe91c1548f1be2e03eeb44905fa2a
cdbrauer <cole.brauer@outlook.com>
2020-02-21 16:02:02 -0700

    Fixes to crop region
    - Resolution-dependent crop region size
    - Improved visualization
    - Reorganized code

--- lane-annotation.py
+++ lane-annotation.py
@@ -2,39 +2,66 @@ import cv2 as cv
 import numpy as np
 # import matplotlib.pyplot as plt
 
-def do_canny(frame):
+# Set the top point of the region of interest as a fraction of the frame (measured from the top)
+top_point_multiplier = 0.5
+
+# Find edges in a frame using canny edge detection
+def detectEdges(frame):
     # Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
     gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
     # Applies a 5x5 gaussian blur with deviation of 0 to frame - not mandatory since Canny will do this for us
     blur = cv.GaussianBlur(gray, (5, 5), 0)
     # Applies Canny edge detector with minVal of 50 and maxVal of 150
-    canny = cv.Canny(blur, 50, 150)
-    return canny
+    frame_edges = cv.Canny(blur, 50, 150)
+    # Return a frame showing all edges
+    return frame_edges
 
-def do_segment(frame):
-    # Since an image is a multi-directional array containing the relative intensities of each pixel in the image, we can use frame.shape to return a tuple: [number of rows, number of columns, number of channels] of the dimensions of the frame
-    # frame.shape[0] give us the number of rows of pixels the frame has. Since height begins from 0 at the top, the y-coordinate of the bottom of the frame is its height
+# Crop a frame to the region of interest
+def polygonMask(frame):
+    # Gets the dimensions of the frame
     height = frame.shape[0]
+    width = frame.shape[1]
     # Creates a triangular polygon for the mask defined by three (x, y) coordinates
-    polygons = np.array([
-                            [(0, height), (800, height), (380, 290)]
-                        ])
+    polygons = np.array([[(0, height), (width, height), (round(width*0.5), round(height*top_point_multiplier))]])
     # Creates an image filled with zero intensities with the same dimensions as the frame
     mask = np.zeros_like(frame)
     # Allows the mask to be filled with values of 1 and the other areas to be filled with values of 0
     cv.fillPoly(mask, polygons, 255)
     # A bitwise and operation between the mask and frame keeps only the triangular area of the frame
-    segment = cv.bitwise_and(frame, mask)
-    return segment
+    frame_cropped = cv.bitwise_and(frame, mask)
+    # Get the coords of the crop region edges
+    edge_coords = np.array([[0, height, round(width*0.5), round(height*top_point_multiplier)], [width, height, round(width*0.5), round(height*top_point_multiplier)]])
+    # Return frame with the mask applied
+    return frame_cropped, edge_coords
 
-def calculate_lines(frame, lines):
+# Calculate the end coordinates of a line given its slope and y intercept
+def calculateEndCoordinates(frame, parameters):
+    slope, intercept = parameters
+    # Gets the dimensions of the frame
+    height = frame.shape[0]
+    width = frame.shape[1]
+    # Sets initial y-coordinate as the bottom of the frame
+    y1 = height
+    # Sets final y-coordinate as the top of the region of interest
+    y2 = round(height*top_point_multiplier)
+    # Sets initial x-coordinate as (y1 - b) / m since y1 = mx1 + b
+    x1 = int((y1 - intercept) / slope)
+    # Sets final x-coordinate as (y2 - b) / m since y2 = mx2 + b
+    x2 = int((y2 - intercept) / slope)
+    # Create an array with the calculated coords and return it
+    coords = np.array([x1, y1, x2, y2])
+    return coords
+
+# Find the left and right lane lines by averaging the detected edges
+def findLaneLines(frame_edges):
+    # Get the endpoints of every detected edge
+    hough = cv.HoughLinesP(frame_edges, 2, np.pi / 180, 100, np.array([]), minLineLength = 100, maxLineGap = 50)
     # Empty arrays to store the coordinates of the left and right lines
     left = []
     right = []
-
     # Loops through every detected line
-    if lines is not None:
-        for line in lines:
+    if hough is not None:
+        for line in hough:
             # Reshapes line from 2D array to 1D array
             x1, y1, x2, y2 = line.reshape(4)
             # Fits a linear polynomial to the x and y coordinates and returns a vector of coefficients which describe the slope and y-intercept
@@ -46,68 +73,69 @@ def calculate_lines(frame, lines):
                 left.append((slope, y_intercept))
             else:
                 right.append((slope, y_intercept))
-
         # Averages out all the values for left and right into a single slope and y-intercept value for each line
         left_avg = np.average(left, axis = 0)
         right_avg = np.average(right, axis = 0)
         # Calculates the x1, y1, x2, y2 coordinates for the left and right lines
-        left_line = calculate_coordinates(frame, left_avg)
-        right_line = calculate_coordinates(frame, right_avg)
-        return np.array([left_line, right_line])
+        left_line = calculateEndCoordinates(frame_edges, left_avg)
+        right_line = calculateEndCoordinates(frame_edges, right_avg)
+        coords = np.array([left_line, right_line])
+    else:
+        coords = np.array([[], []])
+    # Return the endpoint coords of the left and right lines
+    return coords
 
-    return np.array([[], []])
-
-def calculate_coordinates(frame, parameters):
-    slope, intercept = parameters
-    # Sets initial y-coordinate as height from top down (bottom of the frame)
-    y1 = frame.shape[0]
-    # Sets final y-coordinate as 150 above the bottom of the frame
-    y2 = int(y1 - 150)
-    # Sets initial x-coordinate as (y1 - b) / m since y1 = mx1 + b
-    x1 = int((y1 - intercept) / slope)
-    # Sets final x-coordinate as (y2 - b) / m since y2 = mx2 + b
-    x2 = int((y2 - intercept) / slope)
-    return np.array([x1, y1, x2, y2])
-
-def visualize_lines(frame, lines):
+# Draw overlay lines on a frame
+def drawLines(frame, line_coords, color = (0, 255, 0)):
     # Creates an image filled with zero intensities with the same dimensions as the frame
-    lines_visualize = np.zeros_like(frame)
+    overlay = np.zeros_like(frame)
     # Checks if any lines are detected
-    if lines is not None:
-        for x1, y1, x2, y2 in lines:
+    if line_coords is not None:
+        for x1, y1, x2, y2 in line_coords:
             # Draws lines between two coordinates with green color and 5 thickness
-            cv.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    return lines_visualize
+            cv.line(overlay, (x1, y1), (x2, y2), color, 5)
+    frame_overlay = cv.addWeighted(frame, 0.9, overlay, 1, 1)
+    return frame_overlay
+
+# Resize a frame by a scaling factor
+def resizeFrame(frame, scale_factor):
+    width = int(frame.shape[1] * scale_factor)
+    height = int(frame.shape[0] * scale_factor)
+    dim = (width, height)
+    frame_resized = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
+    return frame_resized
 
 # The video feed is read in as a VideoCapture object
 #cap = cv.VideoCapture("input.mp4")
 cap = cv.VideoCapture(0)
 
 while (cap.isOpened()):
+	# Get current frame
     # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
     ret, frame = cap.read()
-    canny = do_canny(frame)
-    cv.imshow("canny", canny)
-    # plt.imshow(frame)
-    # plt.show()
-    segment = do_segment(canny)
-    hough = cv.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength = 100, maxLineGap = 50)
-    # Averages multiple detected lines from hough into one line for left border of lane and one line for right border of lane
+    
+    # Apply edge detection
+    frame_edges = detectEdges(frame)
+    # Apply crop
+    frame_edges_crop, crop_edge_coords = polygonMask(frame_edges)    
+    # Overlay crop edges on frame
+    frame_overlay = drawLines(frame, crop_edge_coords, (0, 0, 255))
+    
     try:
-        lines = calculate_lines(frame, hough)
-        # Visualizes the lines
-        lines_visualize = visualize_lines(frame, lines)
-        cv.imshow("hough", lines_visualize)
-        # Overlays lines on frame by taking their weighted sums and adding an arbitrary scalar value of 1 as the gamma argument
-        output = cv.addWeighted(frame, 0.9, lines_visualize, 1, 1)
-        # Opens a new window and displays the output frame
-        cv.imshow("output", output)
+        # Find lane lines
+        line_coords = findLaneLines(frame_edges_crop)        
+        # Overlay detected lane lines on frame
+        frame_overlay = drawLines(frame_overlay, line_coords)
     except:
         pass
+                
+    # Open a new window and display the output frame
+    cv.imshow("output", resizeFrame(frame_overlay, 0.8))
 
     # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
     if cv.waitKey(10) & 0xFF == ord('q'):
         break
-# The following frees up resources and closes all windows
+        
+# Frees up resources and closes all windows
 cap.release()
 cv.destroyAllWindows()
